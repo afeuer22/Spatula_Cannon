b24fb987f4e79d56054a7acec0edbbe59abc0676
Alexander Feuer <ajfeuer525@gmail.com>
2023-08-18 21:01:00 -0400

    Add project files.

---
+++ SpatulaCannon/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpatulaCannon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            port.Write("B");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            port.Write("F");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            port.Open();
+        }
+    }
+}
